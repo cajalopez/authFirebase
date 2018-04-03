8c44be595e2cc05df3b449f5c20bc768ab0c9616
Carlos lopez <carloslopez8613@gmail.com>
2018-04-03 09:18:47 -0500

    firebase setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './component/reusable';
 
 class App extends Component {
+
+  componentWillMount() {
+    firebase.initializeApp({
+    apiKey: 'AIzaSyCgfctxsDS-p0J8BgGCLZuj5qWSrvS1ZNI',
+    authDomain: 'authreact-2c67c.firebaseapp.com',
+    databaseURL: 'https://authreact-2c67c.firebaseio.com',
+    projectId: 'authreact-2c67c',
+    storageBucket: 'authreact-2c67c.appspot.com',
+    messagingSenderId: '394936485864'
+  });
+  }
+
   render() {
     return (
       <View>
@@ -10,6 +23,7 @@ class App extends Component {
       </View>
     );
   }
+
 }
 
 export default App;
